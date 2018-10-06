fde55d2a33c96186d526433c387c962e71e6b519
Sadi <safwaniftekher@gmail.com>
2018-10-06 11:36:38 +0600

    Add files via upload

---
+++ test (another copy)/travelshoot.ads
@@ -0,0 +1,15 @@
+
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "lldb",
+            "request": "launch",
+            "name": "Debug",
+            "program": "${workspaceRoot}/dist/Debug/CLang-MacOSX/cppapplication_1",
+            "args": ["a"],
+            "cwd": "${workspaceRoot}",
+            "terminal": "integrated"
+        }
+    ]
+}
